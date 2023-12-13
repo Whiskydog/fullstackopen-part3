b72af991adeda2a6f8a6860e07d5772363d2f6b0
Whiskydog <alan.varde@gmail.com>
2023-12-13 14:44:35 -0300

    Solve exercise 3.8

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
+morgan.token('body', (req) => {
+  if (req.method === 'POST') return JSON.stringify(req.body);
+  return '\b';
+});
+
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
 let persons = [
   {
