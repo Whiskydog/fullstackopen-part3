01f334f830985255c97e012a213438b024028d2d
Whiskydog <alan.varde@gmail.com>
2023-12-15 07:21:58 -0300

    Solve exercise 3.17

--- index.js
+++ index.js
@@ -60,6 +60,16 @@ app.get('/api/persons/:id', (req, res) => {
   res.status(404).end();
 });
 
+app.put('/api/persons/:id', (req, res, next) => {
+  const { name, number } = req.body;
+  Person.findByIdAndUpdate(req.params.id, { name, number }, { new: true })
+    .then((person) => {
+      if (person) res.json(person);
+      else res.status(404).end();
+    })
+    .catch((error) => next(error));
+});
+
 app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndDelete(req.params.id)
     .then(() => res.status(204).end())
