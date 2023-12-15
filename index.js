d9a51de4ad786b5eadf27442988e4ad16727215a
Whiskydog <alan.varde@gmail.com>
2023-12-14 22:18:38 -0300

    Solve exercise 3.9

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const cors = require('cors');
 
 morgan.token('body', (req) => {
   if (req.method === 'POST') return JSON.stringify(req.body);
   return '\b';
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(
   morgan(':method :url :status :res[content-length] - :response-time ms :body')
